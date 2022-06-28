db6f67fd0bb42fbf0c459a9d0fe34d7d870cc318
Dylan Schmittle <da.schmittle89@gmail.com>
2022-06-27 21:34:46 -0400

    add basic test for single blocklist

--- cmd/blocklist-fetch/main.go
+++ cmd/blocklist-fetch/main.go
@@ -5,3 +5,20 @@ import (
   "log"
   "net/http"
 )
+
+func main() {
+  fmt.Println(getList("https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt"))
+}
+
+func getList(link string)(string) {
+  res, err := http.Get(link)
+  if err != nil {
+    log.Fatal(err)
+  }
+  content, err := ioutil.ReadALL(res.Body)
+  res.Body.Close()
+  if err != nil {
+    log.Fatal(err)
+  }
+  return string(content)
+}
